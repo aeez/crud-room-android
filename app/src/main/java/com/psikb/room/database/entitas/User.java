9a2df5c388eaa95646a18f4c28b72e42c4d0ac50
aeez <makramfais@gmail.com>
2023-10-31 11:10:33 +0700

    init commit

---
+++ app/src/main/java/com/psikb/room/database/entitas/User.java
@@ -0,0 +1,15 @@
+package com.psikb.room.database.entitas;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class User {
+    @PrimaryKey
+    public  int uid;
+
+    @ColumnInfo(name = "name")
+    public String fullName;
+    public String email;
+}
